d5d980befe1c9d530125c6ef85326db2036eeb6e
Dev Patel <dpatel760@gatech.edu>
2025-06-11 16:55:09 -0400

    backend setup

---
+++ Backend/src/test/java/com/project/ToDoist/ToDoistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ToDoist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
